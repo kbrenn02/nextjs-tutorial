998609598e8d9242f919eb80deb027d6b2eddb52
Kev <102503373+kbrenn02@users.noreply.github.com>
2024-01-16 20:01:15 -0800

    Completed all steps of the tutorial video

--- nextjs-tutorial/app/users/page.tsx
+++ nextjs-tutorial/app/users/page.tsx
@@ -1,16 +1,34 @@
 import React from 'react'
 
-const UsersPage = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users');
-    const users = await res.json();
+interface User {
+    id: number;
+    name: string;
+    email: string;
+}
 
+const UsersPage = async () => {
+    
+    const res = await fetch('https://jsonplaceholder.typicode.com/users', 
+    { cache: 'no-store' });
+    const users: User[] = await res.json();
 
     return (
         <>
             <h1>Users</h1>
-            <ul>
-                {users.map(user => user.)}
-            </ul>
+            <table className='table table-bordered'>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map(user => <tr key={user.id}>
+                        <td>{user.name}</td>
+                        <td>{user.email}</td></tr>)}
+                </tbody>
+                
+            </table>
         </>
     )
 }
