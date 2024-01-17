6b4fe8037cfad68cf42625f96b5ab19b457383b1
Kev <102503373+kbrenn02@users.noreply.github.com>
2024-01-17 00:20:23 -0800

    style the active link in the navbar

--- issue-tracker/app/NavBar.tsx
+++ issue-tracker/app/NavBar.tsx
@@ -1,9 +1,14 @@
+'use client';
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { BsFillBugFill } from "react-icons/bs"; // google search react icons. click the github link, copy the import 
 // from the website and include here. Then call the <BsFillBugFill /> item in the link
+import classnames from 'classnames';
 
 const NavBar = () => {
+    const currentPath = usePathname();
 
     const links = [
         { label: 'Dashboard', href: '/' },
@@ -14,7 +19,9 @@ const NavBar = () => {
         <nav className = "flex space-x-6 border-b mb-5 px-5 h-14 items-center">
             {/* flex allows for horizontal alignment of elements. space-x-6 is space between items. border-b is bottom border.
             mb-5 is a margin at the bottom. h-14 is a heigh of the element. items-center vertically centers the elements */}
+            
             <Link href="/"><BsFillBugFill /></Link>
+
             <ul className="flex space-x-6">
                 {/* because we know we'll have multiple links, we created an array that contains the link 
                 label and the href, then we mapped to the array so we could apply the same code (namely className) 
@@ -22,7 +29,19 @@ const NavBar = () => {
                 {links.map(link => 
                     <Link 
                         key={link.href} 
-                        className='text-zinc-500 hover:text-zinc-800 transition-colors' 
+                        // the ${} allows us to use if statements. This, said another way, is
+                        // if link.href == currentPath, make the text zinc-900, else make the text zinc-500
+                        // This will make it so that the current page is highlighted a different color
+                        // A fun way to style the active link
+                        // className = {`${link.href === currentPath ? 'text-zinc-900' : 'text-zinc-500'} hover:text-zinc-800 transition-colors`}
+                        
+                        // using the classnames import, we are able to make our code cleaner. This says that styling on the
+                        // left of the colon will only occur if the statement on the right of the colon is true
+                        className={classnames({
+                            'text-zinc-900' : link.href === currentPath,
+                            'text-zinc-500' : link.href !== currentPath,
+                            'hover:text-zinc-800 transition-colors' : true
+                        })}
                         href={link.href}>{link.label}
                     </Link>)}
             </ul>
