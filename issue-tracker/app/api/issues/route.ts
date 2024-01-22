177b1e214afad52b6c632931472ec652286d309f
Kev <102503373+kbrenn02@users.noreply.github.com>
2024-01-21 19:42:32 -0800

    Set up Radix UI

--- issue-tracker/app/api/issues/route.ts
+++ issue-tracker/app/api/issues/route.ts
@@ -5,9 +5,11 @@ import prisma from "@/prisma/client";
 
 // In the Issue model in the schema.prisma, every attribute has a default value except for title and description,
 // so we add them here. This is using zod
+// If we look at the Issue model in the schema.prisma file, we only need info for title and description because all the
+// rest have default values
 const createIssueSchema = z.object({
-    title: z.string().min(1).max(255),
-    description: z.string().min(1)
+    title: z.string().min(1).max(255), // string with a minimum of 1 character and a max of 255 characters
+    description: z.string().min(1) // string with a minimum of 1 character
 })
 
 export async function POST(request: NextRequest) {
@@ -15,7 +17,7 @@ export async function POST(request: NextRequest) {
     // have to "await" it to get the body of the request. Because we "await", the funciton needs to by "async"
     const validation = createIssueSchema.safeParse(body); // checking if the request is valid
     if (!validation.success) // checking if the validation was successful
-        return NextResponse.json(validation.error.errors, { status: 400}); // the status 400 means that it was an invalid request
+        return NextResponse.json(validation.error.errors, { status: 400}); // the status 400 means that it was invalid data
     
     const newIssue = await prisma.issue.create({ // to actually POST/Create a new issue (using PostMan), the app needs to be 
         // running on the web, using npm run dev, or it will result in an error and not POST the issue
