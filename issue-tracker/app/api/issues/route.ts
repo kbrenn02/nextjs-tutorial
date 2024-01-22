9472c39a6ec33c7545e35a1ddea3f4438daebbc3
Kev <102503373+kbrenn02@users.noreply.github.com>
2024-01-21 22:32:03 -0800

    Handle errors

--- issue-tracker/app/api/issues/route.ts
+++ issue-tracker/app/api/issues/route.ts
@@ -8,8 +8,8 @@ import prisma from "@/prisma/client";
 // If we look at the Issue model in the schema.prisma file, we only need info for title and description because all the
 // rest have default values
 const createIssueSchema = z.object({
-    title: z.string().min(1).max(255), // string with a minimum of 1 character and a max of 255 characters
-    description: z.string().min(1) // string with a minimum of 1 character
+    title: z.string().min(1, 'Title is required.').max(255), // string with a minimum of 1 character and a max of 255 characters
+    description: z.string().min(1, 'Description is required.') // string with a minimum of 1 character
 })
 
 export async function POST(request: NextRequest) {
@@ -17,7 +17,7 @@ export async function POST(request: NextRequest) {
     // have to "await" it to get the body of the request. Because we "await", the funciton needs to by "async"
     const validation = createIssueSchema.safeParse(body); // checking if the request is valid
     if (!validation.success) // checking if the validation was successful
-        return NextResponse.json(validation.error.errors, { status: 400}); // the status 400 means that it was invalid data
+        return NextResponse.json(validation.error.format(), { status: 400}); // the status 400 means that it was invalid data
     
     const newIssue = await prisma.issue.create({ // to actually POST/Create a new issue (using PostMan), the app needs to be 
         // running on the web, using npm run dev, or it will result in an error and not POST the issue
--- issue-tracker/app/issues/new/page.tsx
+++ issue-tracker/app/issues/new/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import { TextField, Button } from '@radix-ui/themes'; // radix ui has a lot of pre built ui components that we can add
+import { TextField, Button, Callout } from '@radix-ui/themes'; // radix ui has a lot of pre built ui components that we can add
 // into our application. Read documentation to see what all I can add using radix ui
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
 import "easymde/dist/easymde.min.css";
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 interface IssueForm {
     title: string;
@@ -14,26 +15,40 @@ interface IssueForm {
 }
 
 const NewIssuePage = () => {
-    const router = useRouter();
+    const router = useRouter(); // use a router to point to the database (the MySQL one we made)
     const {register, control, handleSubmit} = useForm<IssueForm>();
+    const [error, setError] = useState('');
 
     return (
-        <form 
-          className='max-w-xl space-y-3' 
-          onSubmit={handleSubmit(async (data) => {
-            await axios.post('/api/issues', data);
-            router.push('/issues');
-          })}> 
-            <TextField.Root>
-                <TextField.Input placeholder='Title' {...register('title')}/>
-            </TextField.Root>
-            <Controller 
-                name="description"
-                control={control}
-                render={({ field }) => <SimpleMDE placeholder='Description' {...field}/>}
-            />
-            <Button>Submit New Issue</Button>
-        </form>
+        <div className='max-w-xl'>
+            {error && ( // if there is an error, then we show a separate element (called a Callout) showing there was an error
+                <Callout.Root color="red" className="mb-5">
+                    <Callout.Text>{error}</Callout.Text>
+                </Callout.Root>
+            )}
+            <form 
+            className='space-y-3' 
+            onSubmit={handleSubmit(async (data) => { // this onSubmit funciton is used to post the data to the database the
+                // push the user back to the original users page
+                try {
+                    await axios.post('/api/issues', data);
+                    router.push('/issues');
+                } catch (error) {
+                    setError('An unexpected error occurred.');
+                }
+                
+            })}> 
+                <TextField.Root>
+                    <TextField.Input placeholder='Title' {...register('title')}/>
+                </TextField.Root>
+                <Controller 
+                    name="description"
+                    control={control}
+                    render={({ field }) => <SimpleMDE placeholder='Description' {...field}/>}
+                />
+                <Button>Submit New Issue</Button>
+            </form>
+        </div>
     )
 }
 
